ae7b8d97e8d0b6f01eaa9b800273c2d4abaa13b7
Vadim Sirenko <vitalievichkresh@gmail.com>
2019-05-21 20:17:26 +0300

    solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const thumbs = document.getElementById('thumbs');
-const bigImg = [...document.getElementById('largeImg').children][0];
+const bigImg = document.getElementById('largeImg');
 
 thumbs.onclick = function() {
-  bigImg.src = event.target.src;
-  bigImg.alt = event.target.alt;
-  console.log(event.target.src, event.target.alt);
+  if (event.target.tagName === 'IMG') {
+    bigImg.src = event.target.src;
+    bigImg.alt = event.target.alt;
+  }
 };
